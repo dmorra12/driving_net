02475718fd0db546a12e68edf8daa3f289b33f91
David Morra <dmorra1@jhu.edu>
2016-05-12 20:41:41 -0400

    fixed

--- caffe/data/driving/train_driving_googlenet_quick_finetune_resume.sh
+++ caffe/data/driving/train_driving_googlenet_quick_finetune_resume.sh
@@ -2,5 +2,4 @@
 
 $CAFFE_ROOT/build/tools/caffe train \
     --solver=$CAFFE_ROOT/models/driving_googlenet_finetune/quick_solver.prototxt \
-    --weights=$CAFFE_ROOT/models/bvlc_googlenet/bvlc_googlenet.caffemodel
     --snapshot=/home/user012/caffe/models/driving_googlenet_finetune/driving_googlenet_finetune_quick_iter_520000.solverstate
